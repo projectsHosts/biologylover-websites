a5f8d0ff70a6ded3db19f1f596f8ea7c347c68c2
Yogesh <20bit073@gbu.ac.in>
2026-03-10 16:16:57 +0530

    add class wise

--- src/components/mock-tests/pages/ClassWiseTests.tsx
+++ src/components/mock-tests/pages/ClassWiseTests.tsx
@@ -6,11 +6,23 @@ export default function ClassWiseTests() {
   const navigate = useNavigate();
 
   const classes = [
-    { id: "11-pcb", name: "Class 11 PCB", examType: "CLASS11_PCB" },
-    { id: "11-pcm", name: "Class 11 PCM", examType: "CLASS11_PCM" },
-    { id: "12-pcb", name: "Class 12 PCB", examType: "CLASS12_PCB" },
-    { id: "12-pcm", name: "Class 12 PCM", examType: "CLASS12_PCM" }
-  ];
+  // Class 9
+  { id: "9-science", name: "Class 9 Science", examType: "CLASS9_SCIENCE" },
+  { id: "9-maths",   name: "Class 9 Maths",   examType: "CLASS9_MATHS" },
+
+  // Class 10
+  { id: "10-science", name: "Class 10 Science", examType: "CLASS10_SCIENCE" },
+  { id: "10-maths",   name: "Class 10 Maths",   examType: "CLASS10_MATHS" },
+
+  // Class 11
+  { id: "11-pcb", name: "Class 11 PCB", examType: "CLASS11_PCB" },
+  { id: "11-pcm", name: "Class 11 PCM", examType: "CLASS11_PCM" },
+
+  // Class 12
+  { id: "12-pcb", name: "Class 12 PCB", examType: "CLASS12_PCB" },
+  { id: "12-pcm", name: "Class 12 PCM", examType: "CLASS12_PCM" },
+];
+
 
   return (
     <div className="container">
