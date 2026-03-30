321597de925f6ac6e57e35f04d406b96cceb154f
Yogesh <20bit073@gbu.ac.in>
2026-03-31 02:43:34 +0530

    update competiton banner ui and add about rules and eligiblity

--- src/components/competitions/pages/CompetitionDetails.tsx
+++ src/components/competitions/pages/CompetitionDetails.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback, type Key } from "react"
+import { useEffect, useState, useCallback } from "react"
 import { useParams, useNavigate } from "react-router-dom"
 import {
   checkRegistrationStatus,
